cbdf59ea6abca922c758ca89d5bce79aaef5399c
Alexey <funtic1991@gmail.com>
2024-02-16 00:22:15 +0300

    side Form close to done

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,44 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { useEffect, useRef /*, useState*/ } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+export type TArrowProps = {
+	open: boolean;
+	handleOpen: OnClick;
+};
+
+export const ArrowButton = (arrowProps: TArrowProps) => {
+	const { open, handleOpen } = arrowProps;
+
+	// const [isOpen, setIsOpen] = useState(open);
+	const buttonRef = useRef<HTMLDivElement | null>(null);
+	const arrowRef = useRef<HTMLImageElement | null>(null);
+
+	useEffect(() => {
+		buttonRef.current &&
+			buttonRef.current.classList.toggle(styles.container_open);
+		arrowRef.current && arrowRef.current.classList.toggle(styles.arrow_open);
+	}, [open]);
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={styles.container}
+			onClick={handleOpen}
+			ref={buttonRef}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={styles.arrow}
+				ref={arrowRef}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,69 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Text } from 'components/text';
+import { useEffect, useRef, useState } from 'react';
+import { Select } from '../select';
 import styles from './ArticleParamsForm.module.scss';
+import { RadioGroup } from '../radio-group';
+// import { OnClick } from '../arrow-button/ArrowButton';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const formRef = useRef<HTMLElement | null>(null);
+
+	const handChangeState = () => {
+		setIsOpen(!isOpen);
+	};
+
+	useEffect(() => {
+		formRef.current && formRef.current.classList.toggle(styles.container_open);
+	}, [isOpen]);
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton open={isOpen} handleOpen={handChangeState} />
+			<aside className={styles.container} ref={formRef}>
 				<form className={styles.form}>
+					<Text as='h2' size={31} weight={800} uppercase dynamicLite>
+						задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions}
+						placeholder={'шрифт'}
+						selected={fontFamilyOptions[0]}
+						title={'шрифт'}
+					/>
+					<RadioGroup
+						name={'размер шрифта'}
+						options={fontSizeOptions}
+						selected={fontSizeOptions[0]}
+						title={'размер шрифта'}
+					/>
+					<Select
+						options={fontColors}
+						placeholder={'цвет шрифта'}
+						selected={fontColors[0]}
+						title={'цвет шрифта'}
+					/>
+					<Select
+						options={backgroundColors}
+						placeholder={'цвет фона'}
+						selected={backgroundColors[0]}
+						title={'цвет фона'}
+					/>
+					<Select
+						options={contentWidthArr}
+						placeholder={'ширина контента'}
+						selected={contentWidthArr[0]}
+						title={'ширина контента'}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
